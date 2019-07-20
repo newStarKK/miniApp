e43a75fd85c7fb896872977dbf3b21f24b84ae0d
unknown <903447054@qq.com>
2019-07-20 20:36:53 +0800

    find

--- weather/pages/feedback/feedback.js
+++ weather/pages/feedback/feedback.js
@@ -5,11 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-    flag:false
+    flag:false,
+    max: 200
   },
   sub(){
     this.setData({ flag:true})
   },
+  //字数限制  
+  inputs: function (e) {
+    // 获取输入框的内容
+    var value = e.detail.value;
+    // 获取输入框内容的长度
+    var len = parseInt(value.length);
+
+
+    //最多字数限制
+    if (len > this.data.max) return;
+    // 当输入框内容的长度大于最大长度限制（max)时，终止setData()的执行
+    this.setData({
+      currentWordNumber: len //当前字数  
+    });
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
